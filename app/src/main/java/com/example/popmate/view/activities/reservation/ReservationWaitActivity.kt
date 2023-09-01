b88f3f4735822169321ecfe9a0b208db20c5a8d3
sa46lll <azure394@gmail.com>
2023-09-01 17:04:32 +0900

    feat: 예약 페이지 그리기
    issue: #12

---
+++ app/src/main/java/com/example/popmate/view/activities/reservation/ReservationWaitActivity.kt
@@ -0,0 +1,14 @@
+package com.example.popmate.view.activities.reservation
+
+import android.os.Bundle
+import com.example.popmate.R
+import com.example.popmate.config.BaseActivity
+import com.example.popmate.databinding.ActivityReservationWaitBinding
+
+class ReservationWaitActivity :
+    BaseActivity<ActivityReservationWaitBinding>(R.layout.activity_reservation_wait) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
