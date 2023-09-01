11e9917edbcb4939d4792c62f64f7c7c45e9e40b
kosa-kww <dyffh1031@gmail.com>
2023-09-01 17:51:57 +0900

    feat: 팝업스토어 상세페이지 SNS 추가

--- app/src/main/java/com/example/popmate/view/activities/detail/PopupDetailActivity.kt
+++ app/src/main/java/com/example/popmate/view/activities/detail/PopupDetailActivity.kt
@@ -1,12 +1,7 @@
 package com.example.popmate.view.activities.detail
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil.setContentView
-import androidx.fragment.app.Fragment
 import com.example.popmate.R
 import com.example.popmate.config.BaseActivity
 import com.example.popmate.databinding.ActivityPopupDetailBinding
@@ -30,7 +25,7 @@ class PopupDetailActivity :
 
     private fun setInfoFragment() {
         supportFragmentManager.beginTransaction()
-            .replace(binding.detailMainFrame.id, PopupDetailInfo()).commit()
+            .replace(binding.detailMainFrame.id, PopupDetailInfo.newInstance()).commit()
         binding.run {
             infoBtn.run {
                 setTextColor(ContextCompat.getColor(context, R.color.black))
@@ -44,7 +39,7 @@ class PopupDetailActivity :
     }
     private fun setChatFragment() {
         supportFragmentManager.beginTransaction()
-            .replace(binding.detailMainFrame.id, PopupDetailChat()).commit()
+            .replace(binding.detailMainFrame.id, PopupDetailChat.newInstance()).commit()
         binding.run {
             infoBtn.run {
                 setTextColor(ContextCompat.getColor(context, R.color.tx_light_gray))
--- app/src/main/java/com/example/popmate/view/activities/detail/PopupDetailInfo.kt
+++ app/src/main/java/com/example/popmate/view/activities/detail/PopupDetailInfo.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.example.popmate.R
+import com.example.popmate.databinding.FragmentPopupDetailInfoBinding
+import com.example.popmate.model.data.local.Banner
+import com.example.popmate.view.adapters.BannerAdapter
 
 class PopupDetailInfo : Fragment() {
 
@@ -14,19 +18,44 @@ class PopupDetailInfo : Fragment() {
         fun newInstance() = PopupDetailInfo()
     }
 
-    private lateinit var viewModel: PopupDetailInfoViewModel
+    private lateinit var binding: FragmentPopupDetailInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_popup_detail_info, container, false)
+    ): View {
+        binding = FragmentPopupDetailInfoBinding.inflate(inflater, container, false)
+        val bannersFromApi = getSampleBanner()
+        binding.imageCarousel.adapter = BannerAdapter(bannersFromApi)
+        binding.imageCarousel.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val indicator = binding.indicator
+        indicator.setViewPager(binding.imageCarousel)
+        return binding.root;
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PopupDetailInfoViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun getSampleBanner(): List<Banner> {
+        val sampleData = mutableListOf<Banner>(
+            Banner(
+                id= 1,
+                imgUrl = "url to 1"
+            ),
+            Banner(
+                id= 2,
+                imgUrl = "url to 1"
+            ),
+            Banner(
+                id= 3,
+                imgUrl = "url to 1"
+            ),
+            Banner(
+                id= 4,
+                imgUrl = "url to 1"
+            ),
+            Banner(
+                id= 5,
+                imgUrl = "url to 1"
+            )
+        )
+        return sampleData
     }
-
 }
