8180175ccd17957b380b9d713f3cb0dc01394697
jae-ryong <frogs6225@naver.com>
2023-09-01 18:07:11 +0900

    feat: 구매완료페이지

--- app/src/main/java/com/example/popmate/view/activities/order/OrderPaymentActivity.kt
+++ app/src/main/java/com/example/popmate/view/activities/order/OrderPaymentActivity.kt
@@ -1,12 +1,21 @@
 package com.example.popmate.view.activities.order
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.popmate.R
+import com.example.popmate.databinding.ActivityOrderPaymentBinding
 
 class OrderPaymentActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityOrderPaymentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityOrderPaymentBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order_payment)
+        setContentView(binding.root)
+
+        binding.btnOrderPayment.setOnClickListener {
+            val intent = Intent(this,OrderPaymentCompleteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/popmate/view/activities/order/OrderPaymentCompleteActivity.kt
@@ -0,0 +1,16 @@
+package com.example.popmate.view.activities.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.popmate.R
+import com.example.popmate.config.BaseActivity
+import com.example.popmate.databinding.ActivityOrderPaymentCompleteBinding
+
+class OrderPaymentCompleteActivity : BaseActivity<ActivityOrderPaymentCompleteBinding>(R.layout.activity_order_payment_complete) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
