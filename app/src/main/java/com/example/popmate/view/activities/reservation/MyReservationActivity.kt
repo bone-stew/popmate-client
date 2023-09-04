c0d45f924204adce01c3d527016081a9b5393c5e
sa46lll <azure394@gmail.com>
2023-09-04 17:30:43 +0900

    feat: tabLayout 적용 및 디자인
    issue: #21

--- app/src/main/java/com/example/popmate/view/activities/reservation/MyReservationActivity.kt
+++ app/src/main/java/com/example/popmate/view/activities/reservation/MyReservationActivity.kt
@@ -1,13 +1,12 @@
 package com.example.popmate.view.activities.reservation
 
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.example.popmate.R
 import com.example.popmate.config.BaseActivity
 import com.example.popmate.databinding.ActivityMyReservationBinding
 import com.example.popmate.view.adapters.reservation.ViewPagerFragmentAdapter
 import com.example.popmate.viewmodel.reservation.MyReservationViewModel
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MyReservationActivity :
     BaseActivity<ActivityMyReservationBinding>(R.layout.activity_my_reservation) {
@@ -18,14 +17,25 @@ class MyReservationActivity :
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
-        initTab()
+        initView()
     }
 
-    private fun initTab() {
-        // FragmentStateAdapter 생성
+    private fun initView() {
+        /**
+         * ViewPager2에 Fragment 연결
+         */
         val viewpagerFragmentAdapter = ViewPagerFragmentAdapter(this)
-
-        // ViewPager2에 어댑터 설정
         binding.vpVisit.adapter = viewpagerFragmentAdapter
+
+        /**
+         * TabLayout에 ViewPager2 연결
+         */
+        val tabTextList = listOf("예약전", "예약후", "취소됨")
+
+        val layoutVisitTab = binding.layoutVisitTab
+        val vpVisit = binding.vpVisit
+        TabLayoutMediator(layoutVisitTab, vpVisit) { tab, position ->
+            tab.text = tabTextList[position]
+        }.attach()
     }
 }
