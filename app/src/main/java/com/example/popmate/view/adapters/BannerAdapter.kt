c81e5c977e6882407586e8330311c4ef5f483707
kosa-kww <dyffh1031@gmail.com>
2023-09-05 14:01:55 +0900

    feat: 팝업스토어 상세 백엔드 연동

--- app/src/main/java/com/example/popmate/view/adapters/BannerAdapter.kt
+++ app/src/main/java/com/example/popmate/view/adapters/BannerAdapter.kt
@@ -4,26 +4,36 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.popmate.databinding.RowBannerBinding
 import com.example.popmate.model.data.local.Banner
 
 
-
-
-class BannerAdapter(private var banners: List<Banner>) : RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
+class BannerAdapter(private var banners: List<Banner>) :
+    RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
     private var bannerList: ArrayList<Banner>? = null
 
-    inner class BannerViewHolder(private val binding: RowBannerBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class BannerViewHolder(private val binding: RowBannerBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         init {
-//            binding.root.setOnClickListener{
-//                Toast.makeText(binding.root.context, "클릭된 아이템 = ${binding.banner!!.id}", Toast.LENGTH_LONG).show()
-//            }
+            binding.root.setOnClickListener {
+                Toast.makeText(
+                    binding.root.context,
+                    "클릭된 아이템 = ${binding.banner!!.popupStoreImgId}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
-        fun setItem(banner: Banner){
+
+        fun setItem(banner: Banner) {
             binding.banner = banner
+            Glide.with(binding.bannerImage)
+                .load(banner.imgUrl)
+                .into(binding.bannerImage)
         }
 
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
         val binding = RowBannerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return BannerViewHolder(binding)
