dff9449f128cdbf82d3b947e9191519013497a48
sa46lll <azure394@gmail.com>
2023-09-06 11:45:48 +0900

    feat: 예약 상세 페이지 그리기
    issue: #35

---
+++ app/src/main/java/com/example/popmate/view/activities/reservation/MyReservationDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.popmate.view.activities.reservation
+
+import android.os.Bundle
+import com.example.popmate.R
+import com.example.popmate.config.BaseActivity
+import com.example.popmate.databinding.ActivityPopupDetailBinding
+
+class MyReservationDetailActivity :
+    BaseActivity<ActivityPopupDetailBinding>(R.layout.activity_my_reservation_detail) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/popmate/viewmodel/reservation/MyReservationDetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.popmate.viewmodel.reservation
+
+import com.example.popmate.config.BaseViewModel
+
+class MyReservationDetailViewModel : BaseViewModel() {
+}
