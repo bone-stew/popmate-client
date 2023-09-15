136957271d257d37624ec6e5cd22c19f4bc9c9d6
Kim Woowon <dyffh1031@gmail.com>
2023-09-15 12:15:05 +0900

    feat: chat 페이지네이션 (#95)

--- app/src/main/java/com/example/popmate/model/data/remote/ApiResponse.kt
+++ app/src/main/java/com/example/popmate/model/data/remote/ApiResponse.kt
@@ -5,13 +5,3 @@ data class ApiResponse<T>(
     val message: String,
     val data: T?
 )
-
-fun <T> Result<ApiResponse<T>>.getResult(): Result<T>? {
-    this.onSuccess { response ->
-        val data = response.data ?: return null
-        return Result.success(data)
-    }.onFailure { throwable ->
-        return Result.failure(throwable)
-    }
-    return Result.failure(IllegalStateException("알 수 없는 오류가 발생했습니다."))
-}
