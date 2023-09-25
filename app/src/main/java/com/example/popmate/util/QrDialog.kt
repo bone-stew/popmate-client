7e7b4c668eff3f7e58b6e8e74c6c80922ea66702
jae-ryong <frogs6225@naver.com>
2023-09-25 19:10:51 +0900

    feat : QR 디자인 변경

--- app/src/main/java/com/example/popmate/util/QrDialog.kt
+++ app/src/main/java/com/example/popmate/util/QrDialog.kt
@@ -4,9 +4,11 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.os.CountDownTimer
 import android.view.Window
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.popmate.R
 
@@ -14,8 +16,9 @@ import com.example.popmate.R
 class QrDialog(context: Context, private val qrImgUrl: String) {
     lateinit var listener: LessonOkDialogClickedListener
     lateinit var btnOk: Button
-
+    lateinit var timerTextView: TextView
     private val dlg = Dialog(context)
+    private var countDownTimer: CountDownTimer? = null
 
     interface LessonOkDialogClickedListener {
         fun onOkClicked()
@@ -37,6 +40,31 @@ class QrDialog(context: Context, private val qrImgUrl: String) {
             listener.onOkClicked()
             dlg.dismiss()
         }
+
+        startCountdown()
         dlg.show()
+    }
+
+    private fun startCountdown() {
+        timerTextView = dlg.findViewById(R.id.timer)
+        val totalTime = 15000L // 15초 (밀리초 단위)
+
+        countDownTimer = object : CountDownTimer(totalTime, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                // 1초마다 호출되며, 남은 시간을 표시합니다.
+                val secondsRemaining = millisUntilFinished / 1000
+                timerTextView.text = "$secondsRemaining"
+            }
+
+            override fun onFinish() {
+                // 카운트다운 종료 후 다이얼로그를 닫습니다.
+                dlg.dismiss()
+            }
+        }
+        countDownTimer?.start()
+    }
+
+    fun cancelCountdown() {
+        countDownTimer?.cancel()
     }
 }
